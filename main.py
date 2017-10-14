7b10f6ee141aa95883a64700111e21657d80844b
David Roher <david.roher@gmail.com>
2017-10-14 01:58:09 -0400

    Thread pool downloads and runs all

--- main.py
+++ main.py
@@ -1,97 +1,134 @@
 from subprocess import Popen, PIPE
+from multiprocessing import Pool
+import os
+from enum import Enum
 from lxml import etree
 from lxml.etree import Element
-from typing import Tuple
+import urllib.request
+import json
+import shutil
 import gzip
 import bsdiff4
 
+
 from diachronic.conf import DEFAULT_PATH
 
+URL_PREFIX = "http://dumps.wikimedia.your.org/enwiki/20170901/"
 WIKI_PATH = DEFAULT_PATH + "enwiki-20170901-pages-meta-history27.xml-p42663462p42922763.7z"
-OUTPUT_PATH = DEFAULT_PATH + "bigtest.gzip"
+OUTPUT_PATH = DEFAULT_PATH + "outfiles/"
 WIKI_NS = "{http://www.mediawiki.org/xml/export-0.10/}"
 REVISION_POWER = 1.1
 MAX_REVISION = 1E9
 BUFF_SIZE = 1E8
+THREADS = 4
 
 
-def ltag(elem: Element) -> str:
-    return etree.QName(elem).localname
+class Tags(Enum):
+    Page = "page"
+    Revision = "revision"
+    Timestamp = "timestamp"
+    Title = "title"
+    Text = "text"
+
+    @property
+    def nstag(self) -> str:
+        return WIKI_NS + self.value
+
+    @staticmethod
+    def ltag(elem: Element) -> str:
+        return etree.QName(elem).localname
 
 
-def nstag(tag: str) -> str:
-    return WIKI_NS + tag
+class Diachronic(object):
+    def __init__(self):
+        pass
 
+    def wiki_files(self):
+        json_url = urllib.request.urlopen("{}dumpstatus.json".format(URL_PREFIX))
+        data = json.loads(json_url.read().decode())
+        json_url.close()
+        filenames = data["jobs"]["metahistory7zdump"]["files"].keys()
+        pool = Pool()
+        for filename in filenames:
+            pool.apply_async(self.run, (filename, ))
+        pool.close()
+        pool.join()
+        return 1
 
-def wiki_gen(stream) -> Tuple[str, Element]:
-    for event, elem in etree.iterparse(stream.stdout):
-        yield event, elem
+    @staticmethod
+    def make_valid_indexes(rev_power: float=REVISION_POWER, max_index: int=MAX_REVISION):
+        s = {0}
+        index = 0
+        for i in range(int(max_index)):
+            new_index = int(min(max_index - 1, max(index + 1, rev_power ** i)))
+            if new_index <= index:
+                break
+            s.add(new_index)
+            index = new_index
+        return s
 
+    def run(self, wiki_file):
+        print("Downloading", wiki_file)
+        response = urllib.request.urlopen(URL_PREFIX + wiki_file)
+        download_file = open(DEFAULT_PATH + wiki_file, 'wb')
+        shutil.copyfileobj(response, download_file)
+        response.close()
+        download_file.close()
+        print("Downloaded", wiki_file)
+        wiki_path = DEFAULT_PATH + wiki_file
+        output_path = OUTPUT_PATH + wiki_file + ".gz"
 
-def wiki_stream(path: str) -> Popen:
-    return Popen(["7z", "e", "-so", path], stdout=PIPE)
+        print("Running", wiki_path)
+        global_buffer = bytes()
+        local_buffer = bytes()
+        current_revision = None
+        rev_index = 0
+        print("Making valid indices", wiki_path)
+        valid_indexes = self.make_valid_indexes()
+        print("Opening Popen", wiki_path)
+        stdout = Popen(["7z", "e", "-so", wiki_path], stdout=PIPE).stdout
+        starting = True
 
+        with gzip.open(output_path, 'wb') as f:
+            for event, elem in etree.iterparse(stdout):
+                if starting:
+                    print("Starting read", wiki_path)
+                    starting = False
+                tag = elem.tag
+                if tag == Tags.Revision.nstag:
+                    if rev_index in valid_indexes:
+                        text = bytes(elem.find(Tags.Text.nstag).text or "", 'UTF-8')
+                        if rev_index == 0:
+                            current_revision = text
+                            local_buffer += text
+                        else:
+                            local_buffer += bsdiff4.diff(current_revision, text)
+                            current_revision = text
+                    rev_index += 1
+                elif tag == Tags.Title.nstag:
+                    title = elem.text
+                elif tag == Tags.Page.nstag:
+                    global_buffer += bytes(title, 'UTF-8') + local_buffer
+                    current_revision = None
+                    rev_index = 0
+                    local_buffer = bytes()
 
-def make_valid_indexes(rev_power: float=REVISION_POWER, max_index: int=MAX_REVISION):
-    s = {0}
-    index = 0
-    for i in range(int(max_index)):
-        new_index = int(min(max_index - 1, max(index + 1, rev_power ** i)))
-        if new_index <= index:
-            break
-        s.add(new_index)
-        index = new_index
-    return s
+                    if len(global_buffer) > BUFF_SIZE:
+                        f.write(global_buffer)
+                        global_buffer = bytes()
 
+                    while elem.getprevious() is not None:
+                        del elem.getparent()[0]
 
-def run():
-    global_buffer = bytes()
-    local_buffer = bytes()
-    count = 0
-    valid_indexes = make_valid_indexes()
-    current_revision = None
-    rev_index = 0
-    revs_parsed = 0
-    with gzip.open(OUTPUT_PATH, 'wb') as f:
-        for event, elem in wiki_gen(wiki_stream(WIKI_PATH)):
-            tag = ltag(elem)
-            if tag == "revision":
-                if rev_index in valid_indexes:
-                    text = bytes(elem.find(nstag("text")).text or "", 'UTF-8')
-                    if rev_index == 0:
-                        current_revision = text
-                        local_buffer += text
-                    else:
-                        local_buffer += bsdiff4.diff(current_revision, text)
-                        current_revision = text
-                    revs_parsed += 1
-                rev_index += 1
-            elif tag == "title":
-                title = elem.text
-            elif tag == "page":
-                global_buffer += bytes(title, 'UTF-8') + local_buffer
-                print(count, title, rev_index + 1, revs_parsed, len(global_buffer)/BUFF_SIZE)
+                elem.clear()
 
-                count += 1
-                current_revision = None
-                rev_index = 0
-                revs_parsed = 0
-                local_buffer = bytes()
+            f.write(global_buffer)
+        print("Finished running", wiki_path)
+        os.remove(DEFAULT_PATH + wiki_file)
+        return wiki_path
 
-                if len(global_buffer) > BUFF_SIZE:
-                    print("Writing buffer...")
-                    f.write(global_buffer)
-                    print("Buffer flushed")
-                    global_buffer = bytes()
-
-                while elem.getprevious() is not None:
-                    del elem.getparent()[0]
-
-            elem.clear()
-
-        print("Final write")
-        f.write(global_buffer)
 
 
 if __name__ == "__main__":
-    run()
+    d = Diachronic()
+    d.wiki_files()

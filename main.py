beec965d293ebbf5d6a407c00b5fb2569c03daf0
David Roher <droher@users.noreply.github.com>
2017-10-19 08:02:17 -0400

    Modularize and eliminate diffs (#1)
    * Refactoring into smaller chunks
    
    * Working with download semaphore
    
    * No more diffs

--- main.py
+++ main.py
@@ -2,152 +2,176 @@ import json
 import os
 import shutil
 import urllib.request
-from enum import Enum
-from multiprocessing import Pool
+from typing import List
+from multiprocessing import Semaphore, Pool
 from subprocess import Popen, PIPE
-from timeit import default_timer as timer
-from dateutil.parser import parse as dateparse
 from datetime import datetime, timedelta
+import logging
+import gc
+import psutil
 
 from lxml import etree
-from lxml.etree import Element
 import pyarrow as pa
 import pyarrow.parquet as pq
-import bsdiff4
 from google.cloud import storage
 
-from diachronic.conf import DEFAULT_PATH
+from diachronic import conf, Tags
 
-DATE_INIT = datetime(2001, 1, 15, 0, 0, 0)
-MONTH = '20171001'
-URL_PREFIX = "http://dumps.wikimedia.your.org/enwiki/{}/".format(MONTH)
-OUTPUT_PATH = DEFAULT_PATH + "outfiles/"
-WIKI_NS = "{http://www.mediawiki.org/xml/export-0.10/}"
-REVISION_POWER = 1.1
-MAX_REVISION = 1E9
-BUFF_SIZE = 1E8
-BUCKET_NAME = ""
+DOWNLOAD_SEMAPHORE = Semaphore(conf["download"]["download_parallelism"])
 
 
-class Tags(Enum):
-    Page = "page"
-    Revision = "revision"
-    Timestamp = "timestamp"
-    Title = "title"
-    Text = "text"
-    Namespace = "ns"
-
-    @property
-    def nstag(self) -> str:
-        return WIKI_NS + self.value
-
-    @staticmethod
-    def ltag(elem: Element) -> str:
-        return etree.QName(elem).localname
-
-
-class Diachronic(object):
+class BatchFileHandler(object):
     def __init__(self):
-        pass
+        download = conf["download"]
 
-    def wiki_files(self):
-        json_url = urllib.request.urlopen("{}dumpstatus.json".format(URL_PREFIX))
+        self.input_path = conf["download"]["input_path"]
+        self.output_suffix = conf["upload"]["output_suffix"]
+        self.bucket = conf["upload"]["gcloud_bucket"]
+
+        self.url_prefix = "{}{}/".format(download["url_prefix"],
+                                         download["month_source"])
+
+        self.date_init = conf["run"]["datetime_init"]
+
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+    def get_filenames(self) -> List[str]:
+        json_url = urllib.request.urlopen("{}dumpstatus.json".format(self.url_prefix))
         data = json.loads(json_url.read().decode())
         json_url.close()
-        filenames = list(data["jobs"]["metahistory7zdump"]["files"].keys())
-        pool = Pool()
+        return list(data["jobs"]["metahistory7zdump"]["files"].keys())
+
+    def get_files_to_skip(self) -> List[str]:
         client = storage.Client()
-        bucket = client.get_bucket(BUCKET_NAME)
+        return [blob.name for blob in client.get_bucket(self.bucket).list_blobs()]
 
-        for filename in filenames:
-            if not bucket.blob(filename + ".parquet").exists():
-                pool.apply_async(self.run, (filename, ))
-            else:
-                print("Skipping", filename)
-        pool.close()
-        pool.join()
-        return 1
+    def get_files_to_run(self, overwrite=False) -> List[str]:
+        if overwrite:
+            return self.get_filenames()
+        return [f for f in self.get_filenames()
+                if "{}.{}".format(f, self.output_suffix) in self.get_files_to_skip()]
 
-    def run(self, wiki_file):
-        start = timer()
-        response = urllib.request.urlopen(URL_PREFIX + wiki_file)
-        download_file = open(DEFAULT_PATH + wiki_file, 'wb')
+    def download(self, wiki_file: str) -> None:
+        logging.info("Downloading {}".format(wiki_file))
+        response = urllib.request.urlopen(self.url_prefix + wiki_file)
+        download_file = open(self.input_path + wiki_file, 'wb')
         shutil.copyfileobj(response, download_file)
         response.close()
         download_file.close()
-        end = timer()
-        print("Downloaded {} in {} minutes".format(wiki_file, round((end - start) / 60, 2)))
+        logging.info("Downloaded {}".format(wiki_file))
 
-        start = timer()
-        wiki_path = DEFAULT_PATH + wiki_file
-        output_filename = wiki_file + ".parquet"
+    def run_file(self, wiki_file):
+        with DOWNLOAD_SEMAPHORE:
+            self.download(wiki_file)
+            parser = WikiParser(wiki_file)
+        parser.run()
 
-        # Parquet Table Columns
-        arrow_cols = ("namespace", "title", "initial_text",
-                      "initial_timestamp", "diff_timestamps", "diffs")
-        arrow_buff = {colname: [] for colname in arrow_cols}
+    def run(self):
+        logging.info("Running")
+        filenames_to_run = self.get_files_to_run(overwrite=True)
+        pool = Pool()
+        for f in filenames_to_run:
+            pool.apply_async(self.run_file, args=(f, ))
+        pool.close()
+        pool.join()
 
-        # Tracking iterations
-        arrow_row = {colname: None for colname in arrow_cols}
-        current_revision = None
-        cur_date = DATE_INIT
 
-        stdout = Popen(["7z", "e", "-so", wiki_path], stdout=PIPE).stdout
-        for event, elem in etree.iterparse(stdout):
-            tag = elem.tag
-            if tag == Tags.Revision.nstag:
-                timestamp = dateparse(elem.find(Tags.Timestamp.nstag).text, ignoretz=True)
-                if timestamp >= cur_date and arrow_row["namespace"] == "0":
-                    cur_date = datetime.combine(timestamp.date(), datetime.min.time()) + timedelta(days=1)
-                    text = elem.find(Tags.Text.nstag).text or ""
-                    text_bytes = bytes(text, "UTF-8")
-                    if not current_revision:
-                        current_revision = text_bytes
-                        arrow_row["initial_text"] = text
-                        arrow_row["initial_timestamp"] = timestamp
-                        arrow_row["diff_timestamps"] = []
-                        arrow_row["diffs"] = []
-                    else:
-                        arrow_row["diffs"].append(bsdiff4.diff(current_revision, text_bytes))
-                        arrow_row["diff_timestamps"].append(timestamp)
-                        current_revision = text_bytes
-                elem.clear()
-            elif tag == Tags.Namespace.nstag:
-                arrow_row["namespace"] = elem.text
-            elif tag == Tags.Page.nstag:
-                arrow_row["title"] = elem.find(Tags.Title.nstag).text
-                # Write to buffer and reset trackers
-                if arrow_row["namespace"] == '0':
-                    for col, val in arrow_row.items():
-                        arrow_buff[col].append(val)
-                arrow_row = {colname: None for colname in arrow_cols}
-                current_revision = None
-                cur_date = DATE_INIT
+class WikiParser(object):
+    def __init__(self, wiki_file):
+        self.arrow_cols = ("namespace", "title", "timestamp", "text")
 
-                while elem.getprevious() is not None:
-                    del elem.getparent()[0]
+        self.wiki_file = wiki_file
 
-                elem.clear()
+        self.datetime_init = conf["run"]["datetime_init"]
+        self.bucket = conf["upload"]["gcloud_bucket"]
+        self.input_path = conf["download"]["input_path"]
+        self.output_path = conf["upload"]["output_path"]
+        self.output_file = "{}.{}".format(self.wiki_file, conf["upload"]["output_suffix"])
 
-        arrow_arrays = {colname: pa.array(arr) for colname, arr in arrow_buff.items()}
+        self.arrow_buff = {colname: [] for colname in self.arrow_cols}
+        self.arrow_row, self.cur_date, self.current_revision = self.iter_reset()
+        self.buf_size = 0
+
+        self.schema = None
+        self.writer = None
+
+    def iter_reset(self):
+        self.arrow_row = {colname: None for colname in self.arrow_cols}
+        self.cur_date = self.datetime_init
+        self.current_revision = None
+        return self.arrow_row, self.cur_date, self.current_revision
+
+    @property
+    def func_dict(self):
+        return {
+            Tags.Revision.nstag: self.parse_revision,
+            Tags.Namespace.nstag: self.parse_namespace,
+            Tags.Page.nstag: self.parse_page,
+            Tags.Title.nstag: self.parse_title
+        }
+
+    def parse_title(self, elem) -> None:
+        self.arrow_row["title"] = elem.text
+
+    def parse_namespace(self, elem) -> None:
+        self.arrow_row["namespace"] = elem.text
+
+    def parse_revision(self, elem) -> None:
+        if self.arrow_row["namespace"] == "0":
+            timestamp = datetime.strptime(elem.find(Tags.Timestamp.nstag).text[:-1], "%Y-%m-%dT%H:%M:%S")
+            if timestamp >= self.cur_date:
+                self.cur_date = datetime.combine(timestamp.date(), datetime.min.time()) + timedelta(days=1)
+                text = elem.find(Tags.Text.nstag).text or ""
+                self.arrow_row["text"] = text
+                self.arrow_row["timestamp"] = timestamp
+                for col, val in self.arrow_row.items():
+                    self.buf_size += len(str(val))
+                    self.arrow_buff[col].append(val)
+        elem.clear()
+
+    def parse_page(self, elem) -> None:
+        self.iter_reset()
+        if self.buf_size >= 1E9:
+            self.write()
+        elem.clear()
+
+    def stream(self):
+        stdout = Popen(["7z", "e", "-so", self.input_path + self.wiki_file], stdout=PIPE).stdout
+        for event, elem in etree.iterparse(stdout, huge_tree=True):
+            self.func_dict.get(elem.tag, lambda x: None)(elem)
+
+    def write(self):
+        arrow_arrays = {colname: pa.array(arr) for colname, arr in self.arrow_buff.items()}
         arrow_table = pa.Table.from_arrays(arrays=list(arrow_arrays.values()), names=list(arrow_arrays.keys()))
-        pq.write_table(arrow_table, OUTPUT_PATH + output_filename, compression='brotli')
+        if not self.writer:
+            self.writer = pq.ParquetWriter(self.output_path + self.output_file,
+                                           arrow_table.schema, compression='brotli')
+        self.writer.write_table(arrow_table)
+        self.arrow_buff = {colname: [] for colname in self.arrow_cols}
+        self.buf_size = 0
+        gc.collect()
 
-        # Cloud storage
+    def upload(self):
         client = storage.Client()
-        bucket = client.get_bucket(BUCKET_NAME)
-        blob = bucket.blob(output_filename)
-        with open(OUTPUT_PATH + output_filename, 'rb') as pq_file:
+        bucket = client.get_bucket(self.bucket)
+        blob = bucket.blob(self.output_file)
+        with open(self.output_path + self.output_file, 'rb') as pq_file:
             blob.upload_from_file(pq_file)
 
-        end = timer()
-        os.remove(DEFAULT_PATH + wiki_file)
-        os.remove(OUTPUT_PATH + output_filename)
-        print("Finished {} with shape {} in {} minutes".format(wiki_file,
-                                                               arrow_table.shape, round((end - start) / 60, 2)))
-        return wiki_file
+    def cleanup(self):
+        os.remove(self.input_path + self.wiki_file)
+        os.remove(self.output_path + self.output_file)
+
+    def run(self):
+        logging.info("Started parsing {}".format(self.wiki_file))
+        self.stream()
+        self.write() # Clear leftover buffer
+        self.writer.close()
+        self.upload()
+        self.cleanup()
+        logging.info("Finished parsing {}".format(self.wiki_file))
 
 
 if __name__ == "__main__":
-    d = Diachronic()
-    d.wiki_files()
+    b = BatchFileHandler()
+    b.run()

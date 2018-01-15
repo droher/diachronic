345c74b9ae5d8b8673e250e1692b8adf0a634a8a
David Roher <david.roher@gmail.com>
2018-01-15 15:16:17 -0500

    Support for multiple wikis

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # diachronic
-Wikipedia parser
+
+A parser that turns the revision history dump for a set of wiki sites 
+(e.g. Wikipeida, Wiktionary) into parquet files of daily snapshots.
+
+Uses Apache Arrow for serialization.
+
+The files are uploaded to a specified Google Cloud bucket.
---
+++ diachronic/conf.py
@@ -0,0 +1,28 @@
+import yaml
+from datetime import datetime
+from typing import List
+
+
+class GlobalConf(object):
+    def __init__(self):
+        with open("diachronic/conf.yml", 'rb') as f:
+            conf = yaml.load(f)
+
+        self.wikis: List[str] = conf["wiki_info"]["wikis"]
+        self.month_source: str = conf["wiki_info"]["month_source"]
+        self.namespace: str = conf["wiki_info"]["namespace"]
+        self.datetime_init: datetime = conf["wiki_info"]["datetime_init"]
+
+        self.download_parallelism: int = conf["download"]["download_parallelism"]
+        self.input_path: str = conf["download"]["input_path"]
+        self.url_prefix: str = conf["download"]["url_prefix"]
+
+        self.bucket: str = conf["upload"]["gcloud_bucket"]
+        self.output_path: str = conf["upload"]["output_path"]
+        self.output_suffix: str = conf["upload"]["output_suffix"]
+
+    def get_url_prefix(self, wiki: str) -> str:
+        return "{}{}/{}/".format(self.url_prefix, wiki, self.month_source)
+
+    def get_output_prefix(self, wiki: str) -> str:
+        return "{}/{}/".format(wiki, self.month_source)
